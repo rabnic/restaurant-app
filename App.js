4c1ca7bcba88f90c466a1a999e4574c999c4337e
Nicholas <rabalaond@gmail.com>
2023-10-03 15:50:21 +0200

    Added promotion card on home screen

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import HomeScreen from './screens/HomeScreen';
+import { PaperProvider, Text } from 'react-native-paper';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text className='font-extrabold text-teal-700'>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <HomeScreen />
+    </PaperProvider>
+    // <View style={styles.container}>
+    //   <Text className='font-extrabold text-teal-700'>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
