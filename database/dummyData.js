5376c77291eca23c9d98a2f20c0c0eb8f5f7b45b
Nicholas <rabalaond@gmail.com>
2023-10-04 15:54:33 +0200

    fixed category card issue

---
+++ database/dummyData.js
@@ -0,0 +1,329 @@
+export const menu = {
+    "category4":{
+        "category": "Burgers",
+        "image": "../../assets/menu-images/burgers/mexicano.jpg",
+        "items": [
+            {
+                "name": "Classic Cheeseburger",
+                "image": "../assets/menu-images/classic_cheeseburger.jpg",
+                "price": "$9.99"
+            },
+            {
+                "name": "BBQ Bacon Burger",
+                "image": "../assets/menu-images/bbq_bacon_burger.jpg",
+                "price": "$11.99"
+            },
+            {
+                "name": "Mushroom Swiss Burger",
+                "image": "../assets/menu-images/mushroom_swiss_burger.jpg",
+                "price": "$10.99"
+            },
+            {
+                "name": "Veggie Burger",
+                "image": "../assets/menu-images/veggie_burger.jpg",
+                "price": "$8.99"
+            }
+        ]
+    },
+    "category5":{
+        "category": "Pizzas",
+        "image": "../../assets/menu-images/pizzas/avo-olives.jpg",
+        "items": [
+            {
+                "name": "Margherita Pizza",
+                "image": "../assets/menu-images/margherita_pizza.jpg",
+                "price": "$12.99"
+            },
+            {
+                "name": "Pepperoni Pizza",
+                "image": "../assets/menu-images/pepperoni_pizza.jpg",
+                "price": "$13.99"
+            },
+            {
+                "name": "Vegetarian Pizza",
+                "image": "../assets/menu-images/vegetarian_pizza.jpg",
+                "price": "$11.99"
+            },
+            {
+                "name": "Meat Lovers Pizza",
+                "image": "../assets/menu-images/meat_lovers_pizza.jpg",
+                "price": "$14.99"
+            }
+        ]
+    },
+    "category6":{
+        "category": "Desserts",
+        "image": "../../assets/menu-images/desserts/blued-cream.jpg",
+        "items": [
+            {
+                "name": "Grilled Salmon",
+                "image": "../assets/menu-images/grilled_salmon.jpg",
+                "price": "$15.99"
+            },
+            {
+                "name": "Shrimp Scampi",
+                "image": "../assets/menu-images/shrimp_scampi.jpg",
+                "price": "$14.99"
+            },
+            {
+                "name": "Lobster Bisque",
+                "image": "../assets/menu-images/lobster_bisque.jpg",
+                "price": "$13.99"
+            },
+            {
+                "name": "Crab Cakes",
+                "image": "../assets/menu-images/crab_cakes.jpg",
+                "price": "$16.99"
+            }
+        ]
+    },
+    "category7":{
+        "category": "Platters",
+        "image": "../../assets/menu-images/platters/assorted-platter.jpeg",
+        "items": [
+            {
+                "name": "Chocolate Fondue",
+                "image": "../assets/menu-images/chocolate_fondue.jpg",
+                "price": "$8.99"
+            },
+            {
+                "name": "Tiramisu",
+                "image": "../assets/menu-images/tiramisu.jpg",
+                "price": "$7.99"
+            },
+            {
+                "name": "Cheesecake",
+                "image": "../assets/menu-images/cheesecake.jpg",
+                "price": "$6.99"
+            },
+            {
+                "name": "Apple Pie",
+                "image": "../assets/menu-images/apple_pie.jpg",
+                "price": "$5.99"
+            }
+        ]
+    },
+    "category8":{
+        "category": "Pasta",
+        "image": "../../assets/menu-images/noodles/noodle-3.jpg",
+        "items": [
+            {
+                "name": "Red Wine (Glass)",
+                "image": "../assets/menu-images/red_wine.jpg",
+                "price": "$8.99"
+            },
+            {
+                "name": "White Wine (Glass)",
+                "image": "../assets/menu-images/white_wine.jpg",
+                "price": "$8.99"
+            },
+            {
+                "name": "Mojito",
+                "image": "../assets/menu-images/mojito.jpg",
+                "price": "$7.99"
+            },
+            {
+                "name": "Iced Tea",
+                "image": "../assets/menu-images/iced_tea.jpg",
+                "price": "$3.99"
+            }
+        ]
+    },
+    "category1":{
+        "category": "Colds",
+        "image": "../../assets/menu-images/cold-drinks/assorted.jpg",
+        "items": [
+            {
+                "name": "Bruschetta",
+                "image": "../assets/menu-images/cold-drinks/avatar-sparkling.jpg",
+                "price": "$7.99"
+            },
+            {
+                "name": "Caprese Salad",
+                "image": "../assets/menu-images/cold-drinks/beetie-huice.jpg",
+                "price": "$9.99"
+            },
+            {
+                "name": "Spring Rolls",
+                "image": "../assets/menu-images/cold-drinks/coke.jpg",
+                "price": "$6.99"
+            },
+            {
+                "name": "Stuffed Mushrooms",
+                "image": "../assets/menu-images/cold-drinks/greenie-juice.jpg",
+                "price": "$8.99"
+            },
+            {
+                "name": "Stuffed Mushrooms",
+                "image": "../assets/menu-images/cold-drinks/orangie-juice.jpg",
+                "price": "$8.99"
+            },
+            {
+                "name": "Stuffed Mushrooms",
+                "image": "../assets/menu-images/cold-drinks/sparkling-water.jpg",
+                "price": "$8.99"
+            },
+            {
+                "name": "Stuffed Mushrooms",
+                "image": "../assets/menu-images/cold-drinks/spring-water.jpg",
+                "price": "$8.99"
+            },
+            {
+                "name": "Stuffed Mushrooms",
+                "image": "../assets/menu-images/cold-drinks/tropical-juice.jpg",
+                "price": "$8.99"
+            }
+        ]
+    },
+    "category2":{
+        "category": "Hots",
+        "image": "../../assets/menu-images/hot-drinks/coffee.jpg",
+        "items": [
+            {
+                "name": "Caesar Salad",
+                "image": "../assets/menu-images/caesar_salad.jpg",
+                "price": "$8.99"
+            },
+            {
+                "name": "Greek Salad",
+                "image": "../assets/menu-images/greek_salad.jpg",
+                "price": "$10.99"
+            },
+            {
+                "name": "Cobb Salad",
+                "image": "../assets/menu-images/cobb_salad.jpg",
+                "price": "$9.99"
+            },
+            {
+                "name": "Spinach Salad",
+                "image": "../assets/menu-images/spinach_salad.jpg",
+                "price": "$7.99"
+            }
+        ]
+    },
+    "category3":{
+        "category": "Alcohol",
+        "image": "../../assets/menu-images/alcohol-drinks/heineken.jpg",
+        "items": [
+            {
+                "name": "Spaghetti Carbonara",
+                "image": "../assets/menu-images/spaghetti_carbonara.jpg",
+                "price": "$12.99"
+            },
+            {
+                "name": "Fettuccine Alfredo",
+                "image": "../assets/menu-images/fettuccine_alfredo.jpg",
+                "price": "$11.99"
+            },
+            {
+                "name": "Penne Arrabbiata",
+                "image": "../assets/menu-images/penne_arrabbiata.jpg",
+                "price": "$10.99"
+            },
+            {
+                "name": "Linguine with Clams",
+                "image": "../assets/menu-images/linguine_with_clams.jpg",
+                "price": "$13.99"
+            }
+        ]
+    },
+    "category9":{
+        "category": "Sandwiches",
+        "image": "../../assets/menu-images/platters/sandwiches.jpg",
+        "items": [
+            {
+                "name": "Turkey Club",
+                "image": "../assets/menu-images/turkey_club.jpg",
+                "price": "$9.99"
+            },
+            {
+                "name": "BLT",
+                "image": "../assets/menu-images/blt.jpg",
+                "price": "$8.99"
+            },
+            {
+                "name": "Grilled Cheese",
+                "image": "../assets/menu-images/grilled_cheese.jpg",
+                "price": "$7.99"
+            },
+            {
+                "name": "Vegetarian Wrap",
+                "image": "../assets/menu-images/vegetarian_wrap.jpg",
+                "price": "$8.99"
+            }
+        ]
+    },
+    "category10":{
+        "category": "Rice",
+        "image": "../../assets/menu-images/rice/rice-3.jpg",
+        "items": [
+            {
+                "name": "California Roll",
+                "image": "../assets/menu-images/california_roll.jpg",
+                "price": "$11.99"
+            },
+            {
+                "name": "Spicy Tuna Roll",
+                "image": "../assets/menu-images/spicy_tuna_roll.jpg",
+                "price": "$13.99"
+            },
+            {
+                "name": "Salmon Nigiri",
+                "image": "../assets/menu-images/salmon_nigiri.jpg",
+                "price": "$14.99"
+            },
+            {
+                "name": "Eel Dragon Roll",
+                "image": "../assets/menu-images/eel_dragon_roll.jpg",
+                "price": "$15.99"
+            }
+        ]
+    },
+    "category11":{
+        "category": "Sides",
+        "image": "../../assets/menu-images/sides/fries.jpg",
+        "items": [
+            {
+                "name": "Filet Mignon",
+                "image": "../assets/menu-images/filet_mignon.jpg",
+                "price": "$19.99"
+            },
+            {
+                "name": "Ribeye Steak",
+                "image": "../assets/menu-images/ribeye_steak.jpg",
+                "price": "$18.99"
+            },
+            {
+                "name": "New York Strip",
+                "image": "../assets/menu-images/new_york_strip.jpg",
+                "price": "$17.99"
+            },
+            {
+                "name": "Sirloin Steak",
+                "image": "../assets/menu-images/sirloin_steak.jpg",
+                "price": "$16.99"
+            }
+        ]
+    },
+}  
+
+export const mainPromos = [
+    {
+        image: "../../assets/menu-images/burgers/triple-trouble.jpg",
+        title: "Friends of 3s",
+        discount: "30",
+        extraInfo: "suitable for 3 people",
+    },
+    {
+        image: "../../assets/menu-images/burgers/couple-meal.jpg",
+        title: "",
+        discount: "25",
+        extraInfo: "free drink if you are married",
+    },
+    {
+        image: "../../assets/menu-images/burgers/lunch-spree.jpg",
+        title: "",
+        discount: "20",
+        extraInfo: "sold between 11:00 and 13:00",
+    },
+];
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,142 +1,170 @@
-import { StyleSheet, View, Image, SafeAreaView } from "react-native";
-import React from "react";
+import { StyleSheet, View, Image, SafeAreaView, FlatList, TouchableOpacity } from "react-native";
+import React, { useState, useRef } from "react";
 import { StatusBar } from "expo-status-bar";
 import {
-  Avatar,
-  Searchbar,
-  Card,
-  Text,
-  BottomNavigation,
+    Avatar,
+    Searchbar,
+    Card,
+    Text,
+    BottomNavigation,
+    Button
 } from "react-native-paper";
 import {
-  widthPercentageToDP as wp,
-  heightPercentageToDP as hp,
+    widthPercentageToDP as wp,
+    heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 import Carousel from "react-native-snap-carousel-v4";
 import PromotionCard from "../components/cards/PromotionCard";
+import CategoryCard from "../components/cards/CategoryCard";
+import { menu, mainPromos } from "../database/dummyData";
 
 const HomeScreen = () => {
-  const mainPromos = [
-    {
-      image: "../../assets/menu-images/burgers/triple-trouble.jpg",
-      title: "Friends of 3s",
-      discount: "30",
-      extraInfo: "suitable for 3 people",
-    },
-    {
-      image: "../../assets/menu-images/burgers/couple-meal.jpg",
-      title: "",
-      discount: "25",
-      extraInfo: "free drink if you are married",
-    },
-    {
-      image: "../../assets/menu-images/burgers/lunch-spree.jpg",
-      title: "",
-      discount: "20",
-      extraInfo: "sold between 11:00 and 13:00",
-    },
-  ];
+    const [selectedCategory, setSelectedCategory] = useState('');
+    const [activeIndex, setActiveIndex] = useState(1);
 
-  return (
-    <SafeAreaView className="flex-1 p-4 pt-12 flex-col gap-2">
-      <StatusBar style="auto" />
-      <View className="py-2 flex-row items-center">
-        <Avatar.Image
-          source={require("../assets/user-profile.jpg")}
-          size={40}
-        />
-        <View className="ml-3">
-          <Text>Good afternoon</Text>
-          <Text className="font-bold">Nicholas</Text>
-        </View>
-      </View>
-      <View className="mt-2">
-        <Searchbar
-          placeholder="Search food"
-          onChangeText={() => {}}
-          value={""}
-          className="bg-transparent border border-gray-300"
-        />
-      </View>
-      <Text variant="headlineSmall" className="-mb-2">
-        Promotions
-      </Text>
-      <View className=" flex-row w-full -justify-center">
-        <Card className="w-[95%] bg-slate-600 self-center relative mx-auto">
-          <Card.Cover
-            source={require("../assets/menu-images/burgers/triple-trouble.jpg")}
-          />
-          <View
-            className="w-full h-[100%] absolute flex-col items-center rounded-xl p-2"
-            style={{ backgroundColor: "rgba(0,0,0,.3)" }}
-          >
-            <Text variant="headlineMedium" className="text-white font-bold ">
-              Friends of 3s
-            </Text>
-            <Text variant="bodyLarge" className="text-gray-200 font-bold ">
-              30% off
-            </Text>
-            <Text
-              variant="bodyMedium"
-              className="text-gray-300 font-bold mt-auto"
+    const carouselRef = useRef(null);
+
+    const renderItem = ({ item, index }) => (
+        <Card className="w-[100%] bg-slate-600 self-center relative">
+            <Card.Cover
+                source={require("../assets/menu-images/burgers/triple-trouble.jpg")}
+            />
+            <View
+                className="w-[100%] h-[100%] absolute flex-col items-center rounded-xl p-2"
+                style={{ backgroundColor: "rgba(0,0,0,.3)" }}
             >
-              *suitable for 3 people
-            </Text>
-          </View>
+                <Text variant="headlineMedium" className="text-white font-bold ">
+                    Friends of 3s
+                </Text>
+                <Text variant="bodyLarge" className="text-gray-200 font-bold ">
+                    30% off
+                </Text>
+                <Text
+                    variant="bodyMedium"
+                    className="text-gray-300 font-bold mt-auto"
+                >
+                    *suitable for 3 people
+                </Text>
+            </View>
         </Card>
-      </View>
-      <Text variant="headlineSmall" className="-mb-2">
-        Categories
-      </Text>
-      <View className="flex-row w-[100%] justify-around">
-        <View className="flex-col items-center ">
-          <Avatar.Image
-            source={require("../assets/menu-images/cold-drinks/assorted.jpg")}
-            size={45}
-            className="shadow-lg"
-          />
-          <Text variant="bodyMedium" className="font-extrabold">
-            Drinks
-          </Text>
-        </View>
-        <View className="flex-col items-center">
-          <Avatar.Image
-            source={require("../assets/menu-images/burgers/mexicano.jpg")}
-            size={45}
-          />
-          <Text variant="bodyMedium">Burgers</Text>
-        </View>
-        <View className="flex-col items-center">
-          <Avatar.Image
-            source={require("../assets/menu-images/pizzas/avo-olives.jpg")}
-            size={45}
-          />
-          <Text variant="bodyMedium">Pizzas</Text>
-        </View>
-        <View className="flex-col items-center">
-          <Avatar.Image
-            source={require("../assets/menu-images/noodles/noodle-2.jpg")}
-            size={45}
-          />
-          <Text variant="bodyMedium">Pasta</Text>
-        </View>
-        <View className="flex-col items-center">
-          <Avatar.Image
-            source={require("../assets/menu-images/platters/assorted-platter.jpeg")}
-            size={45}
-          />
-          <Text variant="bodyMedium">Platters</Text>
-        </View>
-        <View className="flex-col items-center">
-          <Avatar.Image
-            source={require("../assets/menu-images/rice/rice-3.jpg")}
-            size={45}
-          />
-          <Text variant="bodyMedium">Rice</Text>
-        </View>
-      </View>
-    </SafeAreaView>
-  );
+    );
+
+    const onSnapToItem = (index) => {
+        setActiveIndex(index);
+    };
+
+
+    const categoryData = () => {
+        return Object.keys(menu).map(key => {
+            return {
+                categoryId: key,
+                category: menu[key].category,
+                image: menu[key].image
+            }
+        })
+    }
+
+    return (
+        <SafeAreaView className="flex-1 pt-12 px-4 flex-col">
+            <StatusBar style="auto" />
+            <View className="py-2 flex-row items-center">
+                <Avatar.Image
+                    source={require("../assets/user-profile.jpg")}
+                    size={40}
+                />
+                <View className="ml-3">
+                    <Text>Good afternoon</Text>
+                    <Text className="font-bold">Nicholas</Text>
+                </View>
+            </View>
+            <View className="mb-4">
+                <Searchbar
+                    placeholder="Search food"
+                    onChangeText={() => { }}
+                    value={""}
+                    className="bg-transparent border border-gray-300"
+                />
+            </View>
+
+            <View className="mb-4 flex-row w-[100%] max-w-[100%] justify-center items-center">
+                <Carousel
+                    layout={"default"}
+                    ref={carouselRef}
+                    data={mainPromos}
+                    sliderWidth={wp(75)}
+                    itemWidth={wp(85)}
+                    renderItem={renderItem}
+                    onSnapToItem={onSnapToItem}
+                />
+                {/* <Card className="w-[100%] bg-slate-600 self-center relative">
+                    <Card.Cover
+                        source={require("../assets/menu-images/burgers/triple-trouble.jpg")}
+                    />
+                    <View
+                        className="w-[100%] h-[100%] absolute flex-col items-center rounded-xl p-2"
+                        style={{ backgroundColor: "rgba(0,0,0,.3)" }}
+                    >
+                        <Text variant="headlineMedium" className="text-white font-bold ">
+                            Friends of 3s
+                        </Text>
+                        <Text variant="bodyLarge" className="text-gray-200 font-bold ">
+                            30% off
+                        </Text>
+                        <Text
+                            variant="bodyMedium"
+                            className="text-gray-300 font-bold mt-auto"
+                        >
+                            *suitable for 3 people
+                        </Text>
+                    </View>
+                </Card> */}
+            </View>
+
+            <View className="mb-4 flex-row w-[100%] justify-between h-auto">
+                <FlatList
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    data={categoryData()}
+                    renderItem={({ item }) => (
+                        <TouchableOpacity onPress={() => setSelectedCategory(item.category)}>
+                            <CategoryCard selectedCategory={selectedCategory} image={item.image} category={item.category} />
+                        </TouchableOpacity>
+                    )}
+                    keyExtractor={item => item.categoryId}
+                />
+            </View>
+            <View className="flex-row flex-1 gap-2 flex-wrap">
+                <Card className="w-[50%] bg-white" style={{ height: hp(20) }}>
+                    <Card.Cover source={require('../assets/menu-images/cold-drinks/coke.jpg')} className="h-[60%]" />
+                    <Card.Content className="mt-2">
+                        <Text variant="bodyLarge">Coca-cola 1 x 300ml Bottle</Text>
+                        <Text variant="bodyMedium" className="font-bold ml-auto">R9.50</Text>
+                    </Card.Content>
+                </Card>
+                <Card className="w-[45%] bg-white" style={{ height: hp(20) }}>
+                    <Card.Cover source={require('../assets/menu-images/cold-drinks/beetie-huice.jpg')} className="h-[60%]" />
+                    <Card.Content className="mt-2">
+                        <Text variant="bodyLarge">Coca-cola 1 x 300ml Bottle</Text>
+                        <Text variant="bodyMedium" className="font-bold ml-auto">R9.50</Text>
+                    </Card.Content>
+                </Card>
+                <Card className="w-[45%] bg-white" style={{ height: hp(20) }}>
+                    <Card.Cover source={require('../assets/menu-images/cold-drinks/sping-water.jpg')} className="h-[60%]" />
+                    <Card.Content className="mt-2">
+                        <Text variant="bodyLarge">Coca-cola 1 x 300ml Bottle</Text>
+                        <Text variant="bodyMedium" className="font-bold ml-auto">R9.50</Text>
+                    </Card.Content>
+                </Card>
+                <Card className="w-[50%] bg-white" style={{ height: hp(20) }}>
+                    <Card.Cover source={require('../assets/menu-images/cold-drinks/greenie-juice.jpg')} className="h-[60%]" />
+                    <Card.Content className="mt-2">
+                        <Text variant="bodyLarge">Coca-cola 1 x 300ml Bottle</Text>
+                        <Text variant="bodyMedium" className="font-bold ml-auto">R9.50</Text>
+                    </Card.Content>
+                </Card>
+            </View>
+        </SafeAreaView>
+    );
 };
 
 export default HomeScreen;
